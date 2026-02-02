5df7c32f057fec2fe4ca2ce1ed966f6583a667fe
AakashCpp <aakashbenarjee110@gmail.com>
2026-02-02 18:05:52 +0530

    Test the env for development

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <>
-      <div>hello blackhole</div>
+      <div className="bg-zinc-950 h-screen w-screen text-white text-center">
+        hello blackhole
+        <div className="cursor-pointer" onClick={increment}>
+          <h1>{`count : ${count}`}</h1>
+        </div>
+      </div>
     </>
   );
 }
