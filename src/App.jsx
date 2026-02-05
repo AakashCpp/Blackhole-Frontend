69cc6ca235df7993845bb86c46e7fe8a89759902
AakashCpp <aakashbenarjee110@gmail.com>
2026-02-06 00:18:46 +0530

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import { TrafficDashboard } from "./components/TrafficDashboard";
 
 function App() {
   return (
     <>
-      <div className="bg-zinc-950 min-h-screen w-screen text-white p-20">
-        <TrafficDashboard />
-      </div>
+      <div className="bg-zinc-950 min-h-screen w-screen text-white p-20"></div>
     </>
   );
 }
