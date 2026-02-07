dfc9d9a3c2f6f0bc3bd36378a3fe52539e27a7b9
AakashCpp <aakashbenarjee110@gmail.com>
2026-02-07 18:02:50 +0530

    hehehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
 import Landing from "./pages/Landing";
 import Navbar from "./components/Navbar";
-import HelmetScene from "./components/HackerMaskScene";
 
 function App() {
   return (
     <>
-      <div className="bg-zinc-950 text-white px-10 h-screen w-screen">
-        {/* <Navbar></Navbar>
-        <Landing></Landing> */}
-        <HelmetScene />
+      <div className="bg-zinc-950 min-h-screen w-screen text-white px-10">
+        <Navbar></Navbar>
+        <Landing></Landing>
       </div>
     </>
   );
