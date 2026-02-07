aa81ab315ec032b63366eb13993d2273b5f04a3d
AakashCpp <aakashbenarjee110@gmail.com>
2026-02-07 17:56:51 +0530

    first 3d model loaded and made a comlete component

---
+++ src/components/HackerMaskScene.jsx
@@ -0,0 +1,32 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF, Environment } from "@react-three/drei";
+import { Suspense } from "react";
+
+function Helmet() {
+  const { scene } = useGLTF("/models/game_ready_scifi_helmet.glb");
+
+  return <primitive object={scene} scale={1.2} position={[0, -0.3, 0]} />;
+}
+
+export default function HelmetScene() {
+  return (
+    <Canvas camera={{ position: [0, 0, 3.5], fov: 40 }}>
+      {/* Lights */}
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[3, 4, 5]} intensity={1.2} />
+
+      <Environment
+        files="/env/ferndale_studio_09_2k.hdr"
+        intensity={0.7}
+        background={false}
+      />
+
+      {/* Suspense is REQUIRED */}
+      <Suspense fallback={null}>
+        <Helmet />
+      </Suspense>
+
+      <OrbitControls />
+    </Canvas>
+  );
+}
