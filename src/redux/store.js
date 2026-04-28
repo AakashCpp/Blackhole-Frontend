b02bd467a6a18f7b775b030e921c9d5423190b59
AakashCpp <aakashbenarjee110@gmail.com>
2026-04-28 14:25:19 +0530

    Something modified on store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import userReducer from "./slices/userSlice";
+import phishReducer from "./slices/phishSlice";
+import jobReducer from "./slices/jobSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    phish: phishReducer,
+    job: jobReducer,
   },
 });
 
